e47b6c440e861730727df325d9acd3eaa1c193c6
kelvin373-ht <kelvin373.ht@gmail.com>
2021-03-10 06:08:53 +0700

    Material 003: Define Variable

---
+++ src/Material003/DefineVariableJava.java
@@ -0,0 +1,12 @@
+package Material003;
+
+public class DefineVariableJava {
+    public static void main(String[] args) {
+        int a = 5;
+        double b = 6.25;
+        char c = 'k';
+        boolean d = true;
+        String e = "Algokelvin";
+    }
+}
+
---
+++ src/Material003/DefineVariableKotlin.kt
@@ -0,0 +1,10 @@
+package Material003
+
+fun main() {
+    val a = 5
+    val b = 6.25
+    val c = 'k'
+    val d = true
+    val e = "Algokelvin"
+}
+
