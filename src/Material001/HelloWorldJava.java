f282c8bbecd3fc17513c7143e9c902cfa71a09c8
kelvin373-ht <kelvin373.ht@gmail.com>
2021-03-09 23:14:48 +0700

    Material 001: Hello World

---
+++ src/Material001/HelloWorldJava.java
@@ -0,0 +1,8 @@
+package Material001;
+
+public class HelloWorldJava {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("I'm learn Java Programming");
+    }
+}
---
+++ src/Material001/HelloWorldKotlin.kt
@@ -0,0 +1,6 @@
+package Material001
+
+fun main() {
+    println("Hello World")
+    println("I'm learn Kotlin Fundamental")
+}
