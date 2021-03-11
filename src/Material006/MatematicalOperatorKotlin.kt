f8c29d349d1f4787f8507b3ea86f5ff139e655bf
kelvin373-ht <kelvin373.ht@gmail.com>
2021-03-12 06:45:29 +0700

    Material 006: Mathematical Operator

---
+++ src/Material006/MatematicalOperatorKotlin.kt
@@ -0,0 +1,14 @@
+package Material006
+
+fun main() {
+    val a = 373
+    val b = 273
+    val c = 10
+    val d = 5
+    val e = 3
+    println(a + b) // operator +
+    println(a - b) // operator -
+    println(c * d) // operator * (times)
+    println(c / d) // operator / (divided by)
+    println(c % e) // operator % (modulo)
+}
