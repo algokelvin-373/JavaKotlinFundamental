1f4fe0a0de59b9dccfce8cc064e0cbfab5f53db7
kelvin373-ht <kelvin373.ht@gmail.com>
2021-03-13 00:12:35 +0700

    Material 008: Increment Decrement

---
+++ src/Material008/IncrementDecrementJava.java
@@ -0,0 +1,11 @@
+package Material008;
+
+public class IncrementDecrementJava {
+    public static void main(String[] args) {
+        int a = 5;
+        a++; // increment
+        System.out.println(a);
+        a--; // decrement
+        System.out.println(a);
+    }
+}
---
+++ src/Material008/IncrementDecrementKotlin.kt
@@ -0,0 +1,9 @@
+package Material008
+
+fun main() {
+    var a = 5
+    a++ // increment
+    println(a)
+    a-- // decrement
+    println(a)
+}
