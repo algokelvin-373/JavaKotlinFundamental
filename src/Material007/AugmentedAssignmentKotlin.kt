2b62d4dc03f48467fadf7986de264b64e97c15f5
kelvin373-ht <kelvin373.ht@gmail.com>
2021-03-12 09:51:19 +0700

    Material 007: Augmented Assignment

---
+++ src/Material007/AugmentedAssignmentKotlin.kt
@@ -0,0 +1,8 @@
+package Material007
+
+fun main() {
+    var a = 5; val b = 6; val c = 4
+    a += b; println(a) // result: 11
+    a -= c; println(a) // result: 7
+    a *= c; println(a) // result: 28
+}
