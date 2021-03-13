085e32f30183ac8d90410270cee19623f7c47b97
kelvin373-ht <kelvin373.ht@gmail.com>
2021-03-14 06:28:02 +0700

    Material 014: Elvis Operator

---
+++ src/Material014/ElvisOperatorJava.java
@@ -0,0 +1,10 @@
+package Material014;
+
+public class ElvisOperatorJava {
+    public static void main(String[] args) {
+        //In Java, Elvis looks like 'Ternary'
+        String a = null;
+        String msg = (a != null) ? "get "+a.length() : "null";
+        System.out.println(msg);
+    }
+}
---
+++ src/Material014/ElvisOperatorKotlin.kt
@@ -0,0 +1,7 @@
+package Material014
+
+fun main() {
+    val a: String? = null
+    val b = a?.length ?: 0
+    println(b)
+}
