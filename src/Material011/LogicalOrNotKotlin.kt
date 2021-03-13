78b18f7cac78e33b77e876b38265474470b2fb17
kelvin373-ht <kelvin373.ht@gmail.com>
2021-03-13 22:58:59 +0700

    Material 011: Logical OR and NOT

---
+++ src/Material011/LogicalOrNotKotlin.kt
@@ -0,0 +1,14 @@
+package Material011
+
+fun main() {
+    val a = true; val b = false
+    //Logical OR
+    println(a || a)
+    println(a || b)
+    println(b || a)
+    println(b || b)
+
+    //Logical NOT
+    println(!a)
+    println(!b)
+}
