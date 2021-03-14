85b6f84df6fa1838a060bd5e65bf61652a881332
kelvin373-ht <kelvin373.ht@gmail.com>
2021-03-14 08:21:07 +0700

    Material 015: String Template

---
+++ src/Material015/StringTemplateJava.java
@@ -0,0 +1,11 @@
+package Material015;
+
+public class StringTemplateJava {
+    public static void main(String[] args) {
+        int a = 5;
+        String b = "Algokelvin";
+
+        System.out.println("Value a: "+a);
+        System.out.println("Length b: "+b.length());
+    }
+}
---
+++ src/Material015/StringTemplateKotlin.kt
@@ -0,0 +1,10 @@
+package Material015
+
+fun main() {
+    val a = 5
+    val b = "Algokelvin"
+
+    println("Value a : "+a)
+    println("Value b : $b")
+    println("Length b : ${b.length}")
+}
