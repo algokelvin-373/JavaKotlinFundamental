070a86e895c1a27930c16e0a4dcf191a191748b9
kelvin373-ht <kelvin373.ht@gmail.com>
2021-03-14 12:04:25 +0700

    Material 016: If Else Expression

---
+++ src/Material016/IfElseExpressionJava.java
@@ -0,0 +1,12 @@
+package Material016;
+
+public class IfElseExpressionJava {
+    public static void main(String[] args) {
+        int a = 6;
+        if (a % 2 == 0) {
+            System.out.println(a+" is even numbers");
+        } else {
+            System.out.println(a+" is odd numbers");
+        }
+    }
+}
---
+++ src/Material016/IfElseExpressionKotlin.kt
@@ -0,0 +1,10 @@
+package Material016
+
+fun main() {
+    val a = 5
+    if (a % 2 == 0) {
+        println("$a is even numbers")
+    } else {
+        println("$a is odd numbers")
+    }
+}
