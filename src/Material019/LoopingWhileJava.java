14fc3e1b302d64f5eac75b2cbc07a5f5fb95d6be
kelvin373-ht <kelvin373.ht@gmail.com>
2021-03-16 03:18:22 +0700

    Material 019: Looping "While"

---
+++ src/Material019/LoopingWhileJava.java
@@ -0,0 +1,13 @@
+package Material019;
+
+public class LoopingWhileJava {
+    public static void main(String[] args) {
+        //Show: 1 2 3 ... 10
+        int x = 1;
+        while(x <= 10) {
+            System.out.print(x+" ");
+            x++;
+        }
+        System.out.println();
+    }
+}
---
+++ src/Material019/LoopingWhileKotlin.kt
@@ -0,0 +1,11 @@
+package Material019
+
+fun main() {
+    //Show: 1 2 3 ... 10
+    var x = 1
+    while(x <= 10) {
+        print("$x ")
+        x++
+    }
+    println()
+}
