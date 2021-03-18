817f1ed21b8f9202b51c69dbbc6870a3bfdf2bd4
kelvin373-ht <kelvin373.ht@gmail.com>
2021-03-18 12:59:59 +0700

    Material 028: Sorted

---
+++ src/Material028/SortedJava.java
@@ -0,0 +1,7 @@
+package Material028;
+
+public class SortedJava {
+    public static void main(String[] args) {
+        int[] a = {32, 11, 25, 73, 44};
+    }
+}
---
+++ src/Material028/SortedKotlin.kt
@@ -0,0 +1,7 @@
+package Material028
+
+fun main() {
+    val a = arrayOf(32, 11, 25, 73, 44)
+    val b = a.sort()
+    println(b)
+}
