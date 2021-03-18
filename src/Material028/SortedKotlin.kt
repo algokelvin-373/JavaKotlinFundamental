2d2dd24b7bcc53eb81cef7b1e11eb57424644021
kelvin373-ht <kelvin373.ht@gmail.com>
2021-03-19 05:53:23 +0700

    Material 028: Sorted Repair

--- src/Material028/SortedKotlin.kt
+++ src/Material028/SortedKotlin.kt
@@ -1,7 +1,11 @@
 package Material028
 
 fun main() {
-    val a = arrayOf(32, 11, 25, 73, 44)
-    val b = a.sort()
+    val a = listOf(32, 11, 25, 73, 44)
+    println("Ascending")
+    val b = a.sorted()
     println(b)
+    println("Descending")
+    val c = a.sortedDescending()
+    println(c)
 }
