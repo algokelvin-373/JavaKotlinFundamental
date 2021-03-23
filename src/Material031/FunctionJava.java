e48bebaea5e177cb862dbd4de992f648d87651e8
kelvin373-ht <kelvin373.ht@gmail.com>
2021-03-23 08:31:17 +0700

    Material 031: Function

---
+++ src/Material031/FunctionJava.java
@@ -0,0 +1,10 @@
+package Material031;
+
+public class FunctionJava {
+    public static void main(String[] args) {
+        functionBasic();
+    }
+    public static void functionBasic() {
+        System.out.println("You call this method basic");
+    }
+}
---
+++ src/Material031/FunctionKotlin.kt
@@ -0,0 +1,8 @@
+package Material031
+
+fun main() {
+    functionBasic()
+}
+fun functionBasic() {
+    println("You call this method basic")
+}
