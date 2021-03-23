510cffca175d02e2906456086512094d3c544040
kelvin373-ht <kelvin373.ht@gmail.com>
2021-03-23 10:27:24 +0700

    Material 032: Function with Parameter

---
+++ src/Material032/FunctionParameterKotlin.kt
@@ -0,0 +1,9 @@
+package Material032
+
+fun main() {
+    val txt = "You call this method using parameter"
+    functionWithParameter(txt)
+}
+fun functionWithParameter(txt: String) {
+    println(txt)
+}
