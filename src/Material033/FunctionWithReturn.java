9b40661c7adb0b9bfb7579c8300b40b21a046b13
kelvin373-ht <kelvin373.ht@gmail.com>
2021-03-24 07:11:51 +0700

    Material 033: Function with Return

---
+++ src/Material033/FunctionWithReturn.java
@@ -0,0 +1,11 @@
+package Material033;
+
+public class FunctionWithReturn {
+    public static void main(String[] args) {
+        String txt = functionWithReturn();
+        System.out.println(txt);
+    }
+    public static String functionWithReturn() {
+        return "You call this function with return";
+    }
+}
---
+++ src/Material033/FunctionWithReturn.kt
@@ -0,0 +1,9 @@
+package Material033
+
+fun main() {
+    val txt = functionWithReturn()
+    println(txt)
+}
+fun functionWithReturn(): String {
+    return "You call this function with return"
+}
