3c0ca4377c0400a314febbd03925ec4f835c246c
kelvin373-ht <kelvin373.ht@gmail.com>
2021-03-26 08:16:32 +0700

    Material 041: Introduction Class

---
+++ src/Material041/ClassKotlin.kt
@@ -0,0 +1,11 @@
+package Material041
+
+fun main() {
+    val kotlinProgram01 = KotlinProgram01()
+    kotlinProgram01.callMethod()
+}
+class KotlinProgram01 {
+    fun callMethod() {
+        println("You call this method")
+    }
+}
