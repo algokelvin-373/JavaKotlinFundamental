9509c19d63eedae1bd029340b49a93cf7ae78f22
kelvin373-ht <kelvin373.ht@gmail.com>
2021-04-25 18:13:47 +0700

    Case Study: Image Encode

--- src/CodingCaseStudy/ImageToByte/ImageToByteJava.java
+++ src/CodingCaseStudy/ImageToByte/ImageToByteJava.java
@@ -6,6 +6,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Base64;
 
 public class ImageToByteJava {
     public static void main(String[] args) throws IOException {
@@ -13,10 +14,13 @@ public class ImageToByteJava {
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
         ImageIO.write(bImage, "png", bos);
         byte [] data = bos.toByteArray();
+        String encodeImg = Base64.getEncoder().encodeToString(data);
 
         System.out.println("Resolution       : "+bImage.getWidth()+" x "+bImage.getHeight());
         System.out.println("Data Byte        : "+data.toString());
         System.out.println("Data Byte Length : "+data.length);
         System.out.println("Data Byte Array  : "+Arrays.toString(data));
+        System.out.println("Data Encode      : "+encodeImg);
+        System.out.println("Data Encode Length : "+encodeImg.length());
     }
 }
--- src/CodingCaseStudy/ImageToByte/ImageToByteKotlin.kt
+++ src/CodingCaseStudy/ImageToByte/ImageToByteKotlin.kt
@@ -10,9 +10,12 @@ fun main() {
     val bos = ByteArrayOutputStream()
     ImageIO.write(bImage, "jpg", bos)
     val data = bos.toByteArray()
+    val encodeImg = Base64.getEncoder().encodeToString(data)
 
     println("Resolution       : " + bImage.width + " x " + bImage.height)
     println("Data Byte        : $data")
     println("Data Byte Length : " + data.size)
     println("Data Byte Array  : " + Arrays.toString(data))
+    println("Data Encode      : $encodeImg")
+    println("Data Encode Length : ${encodeImg.length}")
 }
